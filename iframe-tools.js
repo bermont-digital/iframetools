43edd53c74747f28f9d92e405016b7774a9b2458
Luke <luke@bermont.digital>
2021-10-15 01:11:26 +0200

    Adding changes

--- iframe-tools.js
+++ iframe-tools.js
@@ -2,10 +2,30 @@ class iframeTools {
     constructor() {
 
     }
-
     createCookieWindow(url) {
-        console.log(url);
+        alert('Testing some functionality on NPM' + url);
+    }
+
+    listenWindow(url, selector) {
+        window.addEventListener( "message", function (event) {
+                if (event.origin === url) {
+                    let iframe = document.querySelector(selector);
+                    iframe.style.height = event.data["height"] + 'px';
+                }
+            },
+    false);
+    }
+
+    makeRequestWithUserGesture() {
+        const requestStorageAccess = document.requestStorageAccess();
+        requestStorageAccess.then(
+            function () {
+                // Storage access was granted.
+                console.log('storage access granted');
+            },
+            function (e) {
+                console.log('storage access not granted' + e);
+            }
+        );
     }
 }
-
-export { iframeTools };
