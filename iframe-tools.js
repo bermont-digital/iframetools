c1edef3979c9d9d234cd7fa6d06747908e4e4e75
Luke <luke@bermont.digital>
2021-11-11 21:14:53 +0200

    New patch for iframetools package

--- iframe-tools.js
+++ iframe-tools.js
@@ -1,10 +1,29 @@
 class iframeTools {
+    isSafari = false;
+    isFirefox = false;
     constructor() {
-
+        if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0)
+        {
+            this.isSafari = true;
+        }
+        if (navigator.userAgent.search("Firefox") >= 0)
+        {
+            this.isFirefox = true;
+        }
     }
+
     createOauthWindow(url = 'https://covacglobal.com/dest_bounce.php', name = 'Authorization', width = 500, height = 600, left = 0, top = 0) {
         const options =   `width=${width},height=${height},left=${left},top=${top}`;
-        return window.open(url, name, options);
+        if (this.isSafari || this.isFirefox) {
+            document.hasStorageAccess().then(hasAccess => {
+                if (hasAccess) {
+                    // storage access has been granted already.
+                } else {
+                    return window.open(url, name, options);
+                }
+            });
+        }
+
     }
 
     listenWindow(url, selector) {
